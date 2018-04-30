8330283bc190a8fb25e52888338845f6fc0e470d
Andreas Hilbig <ahilbig@me.com>
2018-04-30 15:17:40 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Installation
+> `npm install --save ./types/threex`
+
+# Summary
+This package contains type definitions for threex.js (http://www.threejsgames.com/extensions ).
+
+# Details
+Subprojects currently in scope: http://www.threejsgames.com/extensions/#threex.domevents
+
+Additional Details
+ * Last updated: Fri, 27 Apr 2018 17:40:54 GMT
+ * Dependencies: 
+ * Global values: THREE
+
+# Credits
+These definitions were written by Andreas Hilbig <https://github.com/ahilbig>
---
+++ index.d.ts
@@ -0,0 +1,42 @@
+// Extra Type definitions for threex.js
+// Project: http://www.threejsgames.com/extensions/
+// Subprojects currently in scope: http://www.threejsgames.com/extensions/#threex.domevents
+// Definitions by: Andreas Hilbig <https://github.com/ahilbig>
+
+import {Camera, Object3D} from "three";
+
+declare module 'threex' {
+
+  export class DomEvents {
+    constructor(camera: Camera, domElement?: HTMLElement);
+
+    object: Camera;
+    domElement: HTMLElement | HTMLDocument;
+
+    // add an event listener for this callback
+    addEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean ): void;
+    // remove an event listener for this callback
+    removeEventListener(obect: Object3D, type: string, listener: (event: Event) => void, withBoundingBox: boolean ): void;
+  }
+
+  export class DragControls {
+    constructor(objects, camera: Camera, domElement?: HTMLElement);
+
+    object: Camera;
+    domElement: HTMLElement | HTMLDocument;
+
+    activate(): void;
+
+    deactivate(): void;
+
+    dispose(): void;
+
+    // EventDispatcher mixins
+    addEventListener(type: string, listener: (event: Event) => void ): void;
+    hasEventListener(type: string, listener: (event: Event) => void): void;
+    removeEventListener(type: string, listener: (event: Event) => void): void;
+    dispatchEvent(event: { type: string; [attachment: string]: any; }): void;
+
+  }
+
+}
